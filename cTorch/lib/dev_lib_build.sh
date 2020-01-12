23061daeaf97b6c876f80a31b68bbd79a6d3fbf7
pcpLiu <zhonghao.liu90@gmail.com>
2020-01-11 22:14:40 -0500

    rm submodules

--- cTorch/lib/dev_lib_build.sh
+++ cTorch/lib/dev_lib_build.sh
@@ -3,8 +3,12 @@
 # jemalloc
 cd ./jemalloc
 ./autogen.sh --with-jemalloc-prefix=je_  --disable-cxx
-make
-make INCLUDEDIR="../../include" install_include
+make && make install
+
+# protobuf
+cd ../protobuf
+./autogen.sh && ./configure && make && make install
 
 # protobuf-c
-cd ./protobuf-c
+cd ../protobuf-c
+./autogen.sh && ./configure && make && make install
