4426e30b2e695e257f840e3c31036b3e3b94beda
pcpLiu <zhonghao.liu90@gmail.com>
2020-01-17 10:47:12 -0500

    Setup basic testing structure involving with CMake & gtest

--- tests/test_list.cpp
+++ tests/test_list.cpp
@@ -1,1 +1,9 @@
-#include <gtest/gtest.h>
+#include "list.h"
+#include "node.h"
+#include "gtest/gtest.h"
+#include <stdlib.h>
+
+TEST(cTorchListTest, test1) {
+  EXPECT_EQ(0, 0);
+  CTorchNode *node = (CTorchNode *)malloc(sizeof(CTorchNode));
+}
