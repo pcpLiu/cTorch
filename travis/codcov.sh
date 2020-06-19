f00e4cfec8b214657d9b2faff0498db1735921f3
pcpLiu <pcpLiu@users.noreply.github.com>
2020-06-18 21:45:18 -0400

    fix codcov

--- travis/codcov.sh
+++ travis/codcov.sh
@@ -1,7 +1,10 @@
 #!bin/bash
 
 # Skip gcov on osx or not using gcc
-if [ "$TRAVIS_OS_NAME" = "osx" || "$TRAVIS_COMPILER" != "gcc"]; then exit 0 ; fi
+if [ "$TRAVIS_OS_NAME" = "osx"] || ["$TRAVIS_COMPILER" != "gcc"]
+then
+    exit 0
+fi
 
 
 # generate gcov
