1b6a8b8a3f04733314b59e800d6f741bed09055f
pcpLiu <pcpLiu@users.noreply.github.com>
2020-06-19 22:39:55 -0400

    Fix uploader command

--- travis/codcov.sh
+++ travis/codcov.sh
@@ -12,4 +12,4 @@ cd cTorch/CMakeFiles/cTorch.dir
 gcov *.gcno  -o .
 
 # upload
-- bash <(curl -s https://codecov.io/bash) -t a304e6b7-f321-4bfc-b622-27bf15ed93b4
+bash <(curl -s https://codecov.io/bash) -t a304e6b7-f321-4bfc-b622-27bf15ed93b4
