c164fc8d9f841f522911c45ab69bec4d88d971c8
pcpLiu <pcpLiu@users.noreply.github.com>
2020-06-25 21:16:15 -0400

    atan2

--- cTorch/operators/default/op_atan2_default.c
+++ cTorch/operators/default/op_atan2_default.c
@@ -6,24 +6,15 @@
 #define _cth_atan2_kernel(op, data_type)                                       \
   do {                                                                         \
     CTorchTensor *input_x = array_at(CTorchTensor)(op->in_bound_tensors, 0);   \
-    data_type *ptr_x = (data_type *)input_x->values;                           \
     CTorchTensor *input_y = array_at(CTorchTensor)(op->in_bound_tensors, 1);   \
-    data_type *ptr_y = (data_type *)input_y->values;                           \
     CTorchTensor *output = array_at(CTorchTensor)(op->out_bound_tensors, 0);   \
+    data_type *ptr_x = (data_type *)input_x->values;                           \
+    data_type *ptr_y = (data_type *)input_y->values;                           \
     data_type *ptr_output = (data_type *)output->values;                       \
-    tensor_size_t N = tensor_1->meta_info->n_elements;                         \
+    tensor_size_t N = input_x->meta_info->n_elements;                          \
                                                                                \
     for (tensor_size_t i = 0; i < N; i++) {                                    \
-      data_type x = ptr_x[i];                                                  \
-      data_type y = ptr_y[i];                                                  \
-      if (x > 0.0) {                                                           \
-        ptr_output[i] = arctan;                                                \
-      } else if (y > 0.0) {                                                    \
-      } else if (y < 0.0) {                                                    \
-      } else if (x < 0.0) {                                                    \
-      } else {                                                                 \
-        FAIL_EXIT(CTH_LOG_ERR, "op_atan2_cpu meets undefined situation");      \
-      }                                                                        \
+      ptr_output[i] = atan2(ptr_y[i], ptr_x[i]);                               \
     }                                                                          \
   } while (0)
 
@@ -31,7 +22,9 @@
  * Computation see wiki: https://en.wikipedia.org/wiki/Atan2
  *
  * Inputs & outputs:
- *    - # of input: 1
+ *    - # of input: 2
+ *        - 0: x
+ *        - 1: y
  *    - # of output: 1
  *    - Input and output should be same dimention and type.
  */
