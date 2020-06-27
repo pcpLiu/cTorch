3f93604bf4c2f845de44614c5378bf116a49c929
pcpLiu <pcpLiu@users.noreply.github.com>
2020-06-27 17:06:43 -0400

    erf

---
+++ tests/ops/erf_op_test.cpp
@@ -0,0 +1,58 @@
+#include <ctgmath>
+
+#include "cTorch/c_torch.h"
+#include "tests/test_util.h"
+#include "gtest/gtest.h"
+
+void test_erf(CTH_BACKEND backend, CTH_TENSOR_DATA_TYPE data_type, float min,
+              float max) {
+  tensor_dim_t dims[] = {100, 100};
+  tensor_dim_t n_dim = sizeof(dims) / sizeof(dims[0]);
+  CTorchNode *op_node = create_dummy_op_node_unary(CTH_OP_ID_erf, dims, n_dim,
+                                                   data_type, min, max);
+  CTorchOperator *op = op_node->conent.op;
+  op_erf_cpu(op);
+
+  if (data_type == CTH_TENSOR_DATA_TYPE_FLOAT_16 ||
+      data_type == CTH_TENSOR_DATA_TYPE_FLOAT_32) {
+    _ele_wise_equal_unary(op, float, EXPECT_FLOAT_EQ, erf);
+  } else if (data_type == CTH_TENSOR_DATA_TYPE_FLOAT_64) {
+    _ele_wise_equal_unary(op, double, EXPECT_DOUBLE_EQ, erf);
+  } else if (data_type == CTH_TENSOR_DATA_TYPE_INT_16) {
+    _ele_wise_equal_unary(op, int16_t, EXPECT_EQ, erf);
+  } else if (data_type == CTH_TENSOR_DATA_TYPE_INT_32) {
+    _ele_wise_equal_unary(op, int32_t, EXPECT_EQ, erf);
+  } else if (data_type == CTH_TENSOR_DATA_TYPE_INT_64) {
+    _ele_wise_equal_unary(op, int64_t, EXPECT_EQ, erf);
+  } else if (data_type == CTH_TENSOR_DATA_TYPE_UINT_8) {
+    _ele_wise_equal_unary(op, uint8_t, EXPECT_EQ, erf);
+  }
+}
+
+TEST(cTorchErfOpTest, testFloat16Default) {
+  test_erf(CTH_BACKEND_DEFAULT, CTH_TENSOR_DATA_TYPE_FLOAT_16, -1.0, 1.0);
+}
+
+TEST(cTorchErfOpTest, testFloat32Default) {
+  test_erf(CTH_BACKEND_DEFAULT, CTH_TENSOR_DATA_TYPE_FLOAT_32, -1.0, 1.0);
+}
+
+TEST(cTorchErfOpTest, testFloat64Default) {
+  test_erf(CTH_BACKEND_DEFAULT, CTH_TENSOR_DATA_TYPE_FLOAT_64, -1.0, 1.0);
+}
+
+TEST(cTorchErfOpTest, testInt16Default) {
+  test_erf(CTH_BACKEND_DEFAULT, CTH_TENSOR_DATA_TYPE_INT_16, -1.0, 1.0);
+}
+
+TEST(cTorchErfOpTest, testInt32Default) {
+  test_erf(CTH_BACKEND_DEFAULT, CTH_TENSOR_DATA_TYPE_INT_32, -1.0, 1.0);
+}
+
+TEST(cTorchErfOpTest, testInt64Default) {
+  test_erf(CTH_BACKEND_DEFAULT, CTH_TENSOR_DATA_TYPE_INT_64, -1.0, 1.0);
+}
+
+TEST(cTorchErfOpTest, testUInt8Default) {
+  test_erf(CTH_BACKEND_DEFAULT, CTH_TENSOR_DATA_TYPE_UINT_8, 0.0, 1.0);
+}
