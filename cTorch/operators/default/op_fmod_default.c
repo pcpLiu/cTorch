f2091a599f9b0a6a9fb552bd237bca3df0a0c92f
pcpLiu <pcpLiu@users.noreply.github.com>
2020-06-27 21:14:34 -0400

    fmod

---
+++ cTorch/operators/default/op_fmod_default.c
@@ -0,0 +1,34 @@
+#include <tgmath.h>
+
+#include "cTorch/operator.h"
+#include "cTorch/operators/default/op_list.h"
+#include "cTorch/operators/default/util.h"
+
+#define _cth_add_kernel(a, b) fmod(b, a)
+
+/**
+ * fmod element wise.
+ *
+ * Op requirement:
+ *    - # of input tensors: 2
+ *        - 0: dividend
+ *        - 1: divisor
+ *    - # of output tensors: 1
+ *        - output:  the input always at index 0
+ */
+void op_fmod_cpu(CTorchOperator *op) {
+  FORCE_OP_INPUT_OUTPUT_TENSOR_NUM(op, 2, 1);
+  OP_FAIL_ON_DTYPE(op, CTH_TENSOR_DATA_TYPE_BOOL);
+
+  CTorchTensor *in_a = array_at(CTorchTensor)(op->in_bound_tensors, 0);
+  CTorchTensor *in_b = array_at(CTorchTensor)(op->in_bound_tensors, 1);
+  CTorchTensor *out = array_at(CTorchTensor)(op->out_bound_tensors, 0);
+  int64_t N = in_a->meta_info->n_elements;
+  _cpu_1d_map_elewise_binary(
+      in_a->values,
+      in_b->values,
+      out->values,
+      in_a->meta_info->data_type,
+      N,
+      _cth_add_kernel);
+}
