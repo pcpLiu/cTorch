60ea36206f7929e51e5794da7405a97322aea3ff
pcpLiu <pcpLiu@users.noreply.github.com>
2020-06-27 21:48:33 -0400

    frac

--- cTorch/operators/default/op_frac_default.c
+++ cTorch/operators/default/op_frac_default.c
@@ -1,36 +1,33 @@
+#include <tgmath.h>
+
 #include "cTorch/operators/default/op_list.h"
 #include "cTorch/operators/default/util.h"
 
-#define _cth_angle_kernel(input_ptr, output_ptr, N, data_type)                 \
+#define _cth_frac_kernel(input_ptr, output_ptr, N, data_type)                  \
   do {                                                                         \
     data_type *input_t = (data_type *)input_ptr;                               \
     data_type *output_t = (data_type *)output_ptr;                             \
+    double holder = 0;                                                         \
     for (int i = 0; i < N; i++) {                                              \
-      output_t[i] = (data_type)((float)input_t[i] / CTH_PI);                   \
+      output_t[i] = (data_type)modf(input_t[i], &holder);                      \
     }                                                                          \
   } while (0)
 
 /**
- * Computes the element-wise angle (in radians) of the given input tensor.
- * Computation: input / Pi
- *
- * Note:
- * During computation, input values will be converted to float first and then
- * final result will be stored into output tensor as its datatype.
+ * Computes the fractional portion of each element in input
  *
  * Inputs & outputs:
  *    - # of input: 1
  *    - # of output: 1
  *    - Input and output should be same dimention and type.
  */
-void op_angle_cpu(CTorchOperator *op) {
+void op_frac_cpu(CTorchOperator *op) {
   FORCE_OP_INPUT_OUTPUT_TENSOR_NUM(op, 1, 1);
   OP_FAIL_ON_DTYPE(op, CTH_TENSOR_DATA_TYPE_BOOL);
-  // TODO: same type force
 
   CTorchTensor *in = array_at(CTorchTensor)(op->in_bound_tensors, 0);
   CTorchTensor *out = array_at(CTorchTensor)(op->out_bound_tensors, 0);
   int64_t N = in->meta_info->n_elements;
   _cpu_1d_map_elewise_unary_generic(
-      in->values, out->values, in->meta_info->data_type, N, _cth_angle_kernel);
+      in->values, out->values, in->meta_info->data_type, N, _cth_frac_kernel);
 }
---
+++ tests/ops/frac_op_test.cpp
@@ -0,0 +1,73 @@
+#include <ctgmath>
+
+#include "cTorch/c_torch.h"
+#include "tests/test_util.h"
+#include "gtest/gtest.h"
+
+#define _verify_frac(op, data_type, expect_fn)                                 \
+  do {                                                                         \
+    CTorchTensor *input = array_at(CTorchTensor)(op->in_bound_tensors, 0);     \
+    CTorchTensor *output = array_at(CTorchTensor)(op->out_bound_tensors, 0);   \
+    data_type *input_t = (data_type *)input->values;                           \
+    data_type *output_t = (data_type *)output->values;                         \
+    double holder = 0;                                                         \
+    for (int i = 0; i < input->meta_info->n_elements; i++) {                   \
+      data_type val = (data_type)modf(input_t[i], &holder);                    \
+      expect_fn(output_t[i], val);                                             \
+    }                                                                          \
+                                                                               \
+  } while (0)
+void test_frac(CTH_BACKEND backend, CTH_TENSOR_DATA_TYPE data_type, float min,
+               float max) {
+  tensor_dim_t dims[] = {100, 100};
+  tensor_dim_t n_dim = sizeof(dims) / sizeof(dims[0]);
+  CTorchNode *op_node = create_dummy_op_node_unary(CTH_OP_ID_frac, dims, n_dim,
+                                                   data_type, min, max);
+  CTorchOperator *op = op_node->conent.op;
+  op_frac_cpu(op);
+
+  if (data_type == CTH_TENSOR_DATA_TYPE_FLOAT_16 ||
+      data_type == CTH_TENSOR_DATA_TYPE_FLOAT_32) {
+    _verify_frac(op, float, EXPECT_FLOAT_EQ);
+  } else if (data_type == CTH_TENSOR_DATA_TYPE_FLOAT_64) {
+    _verify_frac(op, double, EXPECT_DOUBLE_EQ);
+  } else if (data_type == CTH_TENSOR_DATA_TYPE_INT_16) {
+    _verify_frac(op, int16_t, EXPECT_EQ);
+  } else if (data_type == CTH_TENSOR_DATA_TYPE_INT_32) {
+    _verify_frac(op, int32_t, EXPECT_EQ);
+  } else if (data_type == CTH_TENSOR_DATA_TYPE_INT_64) {
+    _verify_frac(op, int64_t, EXPECT_EQ);
+  } else if (data_type == CTH_TENSOR_DATA_TYPE_UINT_8) {
+    _verify_frac(op, uint8_t, EXPECT_EQ);
+  } else if (data_type == CTH_TENSOR_DATA_TYPE_BOOL) {
+    _verify_frac(op, bool, EXPECT_EQ);
+  }
+}
+
+TEST(cTorchFracOpTest, testFloat16Default) {
+  test_frac(CTH_BACKEND_DEFAULT, CTH_TENSOR_DATA_TYPE_FLOAT_16, -1.0, 1.0);
+}
+
+TEST(cTorchFracOpTest, testFloat32Default) {
+  test_frac(CTH_BACKEND_DEFAULT, CTH_TENSOR_DATA_TYPE_FLOAT_32, -1.0, 1.0);
+}
+
+TEST(cTorchFracOpTest, testFloat64Default) {
+  test_frac(CTH_BACKEND_DEFAULT, CTH_TENSOR_DATA_TYPE_FLOAT_64, -1.0, 1.0);
+}
+
+TEST(cTorchFracOpTest, testInt16Default) {
+  test_frac(CTH_BACKEND_DEFAULT, CTH_TENSOR_DATA_TYPE_INT_16, -1.0, 1.0);
+}
+
+TEST(cTorchFracOpTest, testInt32Default) {
+  test_frac(CTH_BACKEND_DEFAULT, CTH_TENSOR_DATA_TYPE_INT_32, -1.0, 1.0);
+}
+
+TEST(cTorchFracOpTest, testInt64Default) {
+  test_frac(CTH_BACKEND_DEFAULT, CTH_TENSOR_DATA_TYPE_INT_64, -1.0, 1.0);
+}
+
+TEST(cTorchFracOpTest, testUInt8Default) {
+  test_frac(CTH_BACKEND_DEFAULT, CTH_TENSOR_DATA_TYPE_UINT_8, 0.0, 1.0);
+}
