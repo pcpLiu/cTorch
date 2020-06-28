b1b85aa33d6aaa20bd2503823aae45d899c852a4
pcpLiu <pcpLiu@users.noreply.github.com>
2020-06-27 23:32:24 -0400

    log

--- cTorch/operators/default/op_log_default.c
+++ cTorch/operators/default/op_log_default.c
@@ -4,14 +4,14 @@
 #include "cTorch/operators/default/util.h"
 
 /**
- * Computes lgamma(x)
+ * Computes log(x)
  *
  * Inputs & outputs:
  *    - # of input: 1
  *    - # of output: 1
  *    - Input and output should be same dimention and type.
  */
-void op_lgamma_cpu(CTorchOperator *op) {
+void op_log_cpu(CTorchOperator *op) {
   FORCE_OP_INPUT_OUTPUT_TENSOR_NUM(op, 1, 1);
   OP_FAIL_ON_DTYPE(op, CTH_TENSOR_DATA_TYPE_BOOL);
   // TODO: same type force
@@ -20,5 +20,5 @@ void op_lgamma_cpu(CTorchOperator *op) {
   CTorchTensor *out = array_at(CTorchTensor)(op->out_bound_tensors, 0);
   int64_t N = in->meta_info->n_elements;
   _cpu_1d_map_elewise_unary(
-      in->values, out->values, in->meta_info->data_type, N, lgamma);
+      in->values, out->values, in->meta_info->data_type, N, log);
 }
