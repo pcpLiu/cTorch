bdaa14afb671f76079f1ce8bdabb3e7fe2940878
pcpLiu <pcpLiu@users.noreply.github.com>
2020-06-29 00:00:35 -0400

    mvlgamma

---
+++ cTorch/operators/default/op_mvlgamma_default.c
@@ -0,0 +1,15 @@
+#include "cTorch/operator.h"
+#include "cTorch/operators/default/op_list.h"
+#include "cTorch/operators/default/util.h"
+
+/**
+ * @brief Computes the multivariate log-gamma function) with dimension p
+ * element-wise
+ *
+ * @param op
+ *
+ * @todo Need implementation
+ */
+void op_mvlgamma_cpu(CTorchOperator *op) {
+  FAIL_EXIT(CTH_LOG_ERR, "op_mvlgamma_cpu not implemented");
+}
