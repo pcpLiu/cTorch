5d675752bb1d88f9ebc390d6d23ca6b5c0c1da02
pcpLiu <pcpLiu@users.noreply.github.com>
2020-07-02 22:41:25 -0400

    try aws
    dd

---
+++ ci/aws_s3.sh
@@ -0,0 +1,28 @@
+################################################################################
+#
+# Install cli
+#
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+################################################################################
+#
+# CP credential
+#
+
+if [ ! -d "~/.aws" ]; then
+    mkdir ~/.aws
+fi
+cp aws_credential ~/.aws/credentials
+
+
+
+################################################################################
+#
+# Pull MKL lib for tests
+#
+mkdir third_party/intel_mkl
+aws s3 sync s3://ctorch.github/intel_mkl third_party/intel_mkl
+
