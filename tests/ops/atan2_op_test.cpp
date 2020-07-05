3a9a9eee16205a41913b6f23a1449eb1a9434423
pcpLiu <pcpLiu@users.noreply.github.com>
2020-07-05 16:13:03 -0400

    fix issue

--- tests/ops/atan2_op_test.cpp
+++ tests/ops/atan2_op_test.cpp
@@ -28,7 +28,7 @@ void test_atan2(CTH_BACKEND backend, CTH_TENSOR_DATA_TYPE data_type, float min,
   } else if (backend == CTH_BACKEND_APPLE) {
 #ifdef BACKEND_APPLE
     op_atan2_apple(op);
-#defin
+#endif
   }
 
   sample_print_triple(
