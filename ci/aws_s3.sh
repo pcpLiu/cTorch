0500daae0ea9850778f03ca7accd4fc3ef0c5f7d
pcpLiu <pcpLiu@users.noreply.github.com>
2020-07-05 12:07:41 -0400

    save s3

--- ci/aws_s3.sh
+++ ci/aws_s3.sh
@@ -3,5 +3,12 @@
 # Pull MKL lib for tests
 #
 cd third_party/
-aws s3 cp s3://ctorch.github/intel_mkl.zip .
-unzip intel_mkl.zip
+mkdir intel_mkl && cd intel_mkl
+
+if [ "$RUNNER_OS" == "Linux" ]; then
+    aws s3 cp s3://ctorch.github/intel_mkl/linux.zip .
+    unzip linux.zip
+else
+    aws s3 cp s3://ctorch.github/intel_mkl/mac.zip .
+    unzip mac.zip
+fi
